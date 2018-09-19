f225aa36d3b3978a6e0543e7fd416fd7de8f9341
Santosh Sutar <san.sutar@gmail.com>
2018-09-18 23:20:11 -0700

    Enable eslint to run as part of tests (#23)

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-require('codemod-cli').runTransform(
+require("codemod-cli").runTransform(
   __dirname,
-  process.argv[2]       /* transform name */,
+  process.argv[2] /* transform name */,
   process.argv.slice(2) /* paths or globs */
-)
+);
