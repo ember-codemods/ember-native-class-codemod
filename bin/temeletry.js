f8f942fe467395536f84463ff5c3d015e32337c5
Chris Garrett <cgarrett@linkedin.com>
2019-06-17 16:34:47 -0700

    Add telemetry gathering via puppeteer

--- bin/temeletry.js
+++ bin/temeletry.js
@@ -4,9 +4,3 @@
 const gatherTelemetry = require("../lib/gather-telemetry");
 
 gatherTelemetry(process.argv[2]);
-
-require("codemod-cli").runTransform(
-  __dirname,
-  "ember-object",
-  process.argv.slice(2) /* paths or globs */
-);
