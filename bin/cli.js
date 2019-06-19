008e0e7dae4c85e67a61fe5c91ff68034789b7e6
Chris Garrett <cgarrett@linkedin.com>
2019-06-19 16:15:11 -0700

    [BUGFIX] Wait for telemetry (#126)
    Async functions are async

--- bin/cli.js
+++ bin/cli.js
@@ -3,10 +3,12 @@
 
 const gatherTelemetry = require("../lib/gather-telemetry");
 
-gatherTelemetry(process.argv[2]);
+(async () => {
+  await gatherTelemetry(process.argv[2]);
 
-require("codemod-cli").runTransform(
-  __dirname,
-  "ember-object",
-  process.argv.slice(2) /* paths or globs */
-);
+  require("codemod-cli").runTransform(
+    __dirname,
+    "ember-object",
+    process.argv.slice(2) /* paths or globs */
+  );
+})();
