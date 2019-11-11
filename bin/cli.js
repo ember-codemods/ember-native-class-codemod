767ec7708ee0650a367a7ad35271e06039931296
Pat O'Callaghan <pat@intercom.io>
2019-11-11 18:43:57 +0000

    Correctly parse boolean flags options into boolean values

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 'use strict';
+const argv = require('yargs')
+  .boolean(['class-fields', 'decorators', 'classic-decorator'])
+  .string(['type', 'quote']).argv;
 
 const { gatherTelemetryForUrl, analyzeEmberObject } = require('ember-codemods-telemetry-helpers');
 
 (async () => {
-  await gatherTelemetryForUrl(process.argv[2], analyzeEmberObject);
+  await gatherTelemetryForUrl(argv[2], analyzeEmberObject);
 
   require('codemod-cli').runTransform(
     __dirname,
     'ember-object',
-    process.argv.slice(2) /* paths or globs */
+    argv.slice(2) /* paths or globs */
   );
 })();
