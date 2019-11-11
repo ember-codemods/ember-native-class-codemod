770718cc51e22b7f1f149e193ab1cd2e69e87b86
Pat O'Callaghan <pat@intercom.io>
2019-11-11 22:30:10 +0000

    Spike out params

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,50 @@
 #!/usr/bin/env node
 'use strict';
-const argv = require('yargs')
-  .boolean(['class-fields', 'decorators', 'classic-decorator'])
-  .string(['type', 'quote']).argv;
 
 const { gatherTelemetryForUrl, analyzeEmberObject } = require('ember-codemods-telemetry-helpers');
 
-(async () => {
-  await gatherTelemetryForUrl(argv[2], analyzeEmberObject);
+const argv = require('yargs').command(
+  '$0 <url>',
+  'A codemod for transforming your ember app code to native JavaScript class syntax with decorators!',
+  yargs => {
+    yargs
+      .positional('url', {
+        describe: 'the path to the server',
+        type: 'string',
+      })
+      .option('class-fields', {
+        description: 'Enable/disable transformation using class fields',
+        type: 'boolean',
+        default: true,
+      })
+      .option('decorators', {
+        description: 'Enable/disable transformation using decorators',
+        type: 'boolean',
+        default: true,
+      })
+      .option('classic-decorator', {
+        description:
+          'Enable/disable adding the @classic decorator, which helps with transitioning Ember Octane',
+        type: 'boolean',
+        default: true,
+      })
+      .option('type', {
+        description:
+          'Apply transformation to only passed type. The type can be one of services, routes, components, controllers',
+        type: 'string',
+        default: '',
+      })
+      .option('quote', {
+        description:
+          'Whether to use double or single quotes by default for new statements that are added during the codemod.',
+        type: 'string',
+        default: 'single',
+      });
+  }
+).argv;
 
-  require('codemod-cli').runTransform(
-    __dirname,
-    'ember-object',
-    argv.slice(2) /* paths or globs */
-  );
+(async () => {
+  await gatherTelemetryForUrl(argv.url, analyzeEmberObject);
+
+  require('codemod-cli').runTransform(__dirname, 'ember-object', argv._ /* paths or globs */);
 })();
