e6e9c761065de7b0e8dfe4c43896b671be0ee714
Krystan HuffMenne <krystan@tilde.io>
2023-02-28 13:07:39 -0800

    Upgrade winston

--- transforms/helpers/log-helper.ts
+++ transforms/helpers/log-helper.ts
@@ -1,9 +1,12 @@
 import { createLogger, format, transports } from 'winston';
+import { assert } from './util/types';
 
 const { combine, timestamp, printf } = format;
 
-const logFormatter = printf((info) => {
-  return `${info['timestamp'] as string} [${info.level}] ${info.message}`;
+const logFormatter = printf(({ level, timestamp, message }) => {
+  assert(typeof timestamp === 'string');
+  assert(typeof message === 'string');
+  return `${timestamp} [${level}] ${message}`;
 });
 
 const logger = createLogger({
