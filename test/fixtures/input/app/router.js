53e01ed138f75048a9f25945e852541cca42766d
Krystan HuffMenne <krystan@tilde.io>
2023-04-24 12:07:49 -0700

    Fix fixture failures

--- test/fixtures/input/app/router.js
+++ test/fixtures/input/app/router.js
@@ -1,9 +1,11 @@
 import EmberRouter from '@ember/routing/router';
-import config from 'input/config/environment';
+import config from './config/environment';
 
-export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
-}
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL,
+});
 
 Router.map(function () {});
+
+export default Router;
--- test/fixtures/output/app/router.js
+++ test/fixtures/output/app/router.js
@@ -8,6 +8,6 @@ class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function() {});
+Router.map(function () {});
 
 export default Router;
