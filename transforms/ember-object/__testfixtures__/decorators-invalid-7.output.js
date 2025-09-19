920772661daed2c2defd57bad9c39e853126024c
Buck Doyle <b@chromatin.ca>
2025-09-19 14:07:01 +0200

    Fix self-links to issues and documentation

--- transforms/ember-object/__testfixtures__/decorators-invalid-7.output.js
+++ transforms/ember-object/__testfixtures__/decorators-invalid-7.output.js
@@ -1,7 +1,7 @@
 /*
 Expect error:
   ValidationError: Validation errors for class 'Foo7':
-    [actions]: Transform not supported - [click]: action name matches one of the lifecycle hooks. Rename and try again. See https://github.com/scalvert/ember-native-class-codemod/issues/34 for more details
+    [actions]: Transform not supported - [click]: action name matches one of the lifecycle hooks. Rename and try again. See https://github.com/ember-codemods/ember-native-class-codemod/issues/34 for more details
 */
 
 // Do not transform as action name matches lifecycle hook
